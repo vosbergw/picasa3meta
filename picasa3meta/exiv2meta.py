e2d91222b31d1b0df1a4fbeb7cfde9ea581fb05d
Wayne Vosberg <wayne@mindtunnel.com>
2012-08-29 13:44:46 +0200

    update doc strings and made the contacts db optional for iniinfo

--- picasa3meta/exiv2meta.py
+++ picasa3meta/exiv2meta.py
@@ -20,42 +20,62 @@ import pyexiv2
 
 
 def EXIV2Meta(img):
-    '''
-    Return a list of all the EXIF, IPTC and XMP metadata in the file 'iimg'
-    
-    '''
-    
-    try:
-        metadata = pyexiv2.ImageMetadata(img)
-        metadata.read()
-    except:
-        return zip( ['error'],['%s is not an image'%img])
-    else:
-        ret = []
-        for K in metadata.exif_keys:
-            try:
-                ret.append(metadata[K].human_value)
-            except:
-                ret.append(metadata[K].raw_value)
-        for K in metadata.iptc_keys:
-            try:
-                ret.append(metadata[K].raw_value[0])
-            except:
-                ret.append(metadata[K].value[0])
-        for K in metadata.xmp_keys:
-            if type(metadata[K].raw_value) == dict:
-                nret=""
-                for KK in metadata[K].raw_value:
-                    if len(nret) > 1:
-                        nret = nret+","
-                    nret = nret+metadata[K].raw_value[KK]
-                ret.append(nret)
-            else:
-                try:
-                    ret.append(metadata[K].raw_value)
-                except:
-                    ret.append(metadata[K].value)
-         
-        return zip(
-            metadata.exif_keys+metadata.iptc_keys+metadata.xmp_keys, ret )
-    
+	'''
+
+	Return a list of tuples of all the EXIF, IPTC and XMP metadata in a file.
+	
+	Usage:
+		from picasa3meta import exiv2meta
+
+		metaData = exiv2meta.EXIV2Meta("/path/to/file.jpg")
+
+		for key,value in metaData:
+			print "%s : %s"%(key,value)
+
+	Exif keys will return the human_value if possible, otherwise the raw_value.
+
+	Iptc keys will return the raw_value first, if that fails, just value
+
+	Xmp keys may be dict objects.  If it is a dict, return a comma separated
+	list of the values.  Otherwise, try the raw_value first, then just value.
+
+	'''
+	
+	try:
+		metadata = pyexiv2.ImageMetadata(img)
+		metadata.read()
+	except:
+		return zip( ['error'],['%s is not an image'%img])
+	else:
+		ret = []
+
+		for K in metadata.exif_keys:
+			try:
+				ret.append(metadata[K].human_value)
+			except:
+				ret.append(metadata[K].raw_value)
+
+		for K in metadata.iptc_keys:
+			try:
+				ret.append(metadata[K].raw_value[0])
+			except:
+				ret.append(metadata[K].value[0])
+
+		for K in metadata.xmp_keys:
+			if type(metadata[K].raw_value) == dict:
+				# if the xmp key is a dict, return it as a comma separated list.
+				nret=""
+				for KK in metadata[K].raw_value:
+					if len(nret) > 1:
+						nret = nret+","
+					nret = nret+metadata[K].raw_value[KK]
+				ret.append(nret)
+			else:
+				try:
+					ret.append(metadata[K].raw_value)
+				except:
+					ret.append(metadata[K].value)
+		 
+		# zip the keys and values into a list of tuples and return it
+		return zip(metadata.exif_keys+metadata.iptc_keys+metadata.xmp_keys, ret)
+	
